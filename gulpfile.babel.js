eb7170e8d79046d3ce042cc4f4660ead918ee47f
m.sakamaki <mail@m-sakamaki.jp>
2015-12-25 19:12:02 +0900

    suported node v0.12

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,30 +10,34 @@ const config = {
 };
 
 // before very long...
-gulp.task('eslint', () => gulp.src(config.codes)
+gulp.task('eslint', function() {
+  return gulp.src(config.codes)
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError()));
+    .pipe(eslint.failAfterError())
+  });
 
-gulp.task('jscs', () => gulp.src(config.codes)
-        .pipe(jscs({fix: false})));
+gulp.task('jscs', function() {
+  return gulp.src(config.codes)
+        .pipe(jscs({fix: false}));
+});
 
-gulp.task('build:src', () => del('lib/*')
-    .then(()=> gulp.src('src/index.js')
+gulp.task('build:src', function() {
+  return del('lib/*')
+    .then(function() {
+      return gulp.src('src/index.js')
   		.pipe(babel())
-  		.pipe(gulp.dest('lib'))));
+      .pipe(gulp.dest('lib'));
+  });
+});
 
-gulp.task('build:test', () => del('.tmp/*')
-    .then(()=> gulp.src('test/*.js')
+gulp.task('build:test', function() {
+  return del('.tmp/*')
+    .then(function() {
+      return gulp.src('test/*.js')
   		.pipe(babel())
-  		.pipe(gulp.dest('.tmp'))));
+      .pipe(gulp.dest('.tmp'));
+  });
+});
 
 gulp.task('build', ['jscs', 'build:src','build:test']);
-
-gulp.task('test', function (cb) {
-  exec('node_modules/.bin/mocha .tmp/test.js', (err, stdout, stderr)=> {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
